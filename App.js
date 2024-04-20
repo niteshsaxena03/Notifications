1c2936bdebb46dd22005521a942ccd915c25eea7
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-20 15:10:27 +0530

    Using useEffect to check received notifications

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import * as Notifications from "expo-notifications";
+import {useEffect} from 'react';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -10,6 +11,16 @@ Notifications.setNotificationHandler({
   }),
 });
 export default function App() {
+  useEffect(()=>{
+    const subscription=Notifications.addNotificationReceivedListener((notification)=>{
+      console.log('Notification Received');
+      console.log(notification);
+    });
+    return ()=>{
+      subscription.remove();
+    }
+  },[])
+
   function scheduleNotificationHandler(){
     Notifications.scheduleNotificationAsync({
       content:{
